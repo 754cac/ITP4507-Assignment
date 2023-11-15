0735ab98c8cc7cf81a849bfda5d8ea9b5e40a415
754cac <88232153+754cac@users.noreply.github.com>
2023-11-15 21:34:11 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,42 +6,52 @@ import Factorys.*;
 import Memento.*;
 
 public class Main {
+    // declares a `Scanner` object named `sc` for user input.
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        // - It creates a `Vector` called `Teams` to store instances of the `Team`
+        // class.
         Vector<Team> Teams = new Vector<Team>();
+        // - It creates two stacks of `Command` objects named `undos` and `redos`.
         Stack<Command> undos = new Stack<Command>();
         Stack<Command> redos = new Stack<Command>();
+        // - It creates a `Caretaker` object named `ct` for handling mementos (used
+        // later for undo and redo functionality).
         Caretaker ct = new Caretaker();
-
+        // - It declares an array of `Team` objects called `currTeam` to store the
+        // current team (used as a reference to the current team being manipulated).
         // Change in Command have not feedback, must use array
         Team[] currTeam = new Team[1];
-
         String command;
         Command com;
-
+        // - `createTeamF`: Maps strings ("v" and "f") to `TeamFactory` objects
+        // (`VolleyballTeamFactory` and `FootballTeamFactory`).
         HashMap<String, TeamFactory> createTeamF = new HashMap<String, TeamFactory>();
         createTeamF.put("v", new VolleyballTeamFactory());
         createTeamF.put("f", new FootballTeamFactory());
-
+        // - `createTeamString`: Maps `Class` objects (`VolleyballTeam` and
+        // `FootballTeam`) to strings ("Volleyball" and "Football").
         HashMap<Class<?>, String> createTeamString = new HashMap<Class<?>, String>();
         VolleyballTeam v = new VolleyballTeam(null);
         FootballTeam f = new FootballTeam(null);
-
         createTeamString.put(v.getClass(), "Volleyball");
         createTeamString.put(f.getClass(), "Football");
-
+        // - `addPlayerPromptString`: Maps `Class` objects to strings representing
+        // prompts for adding players.
         HashMap<Class<?>, String> addPlayerPromptString = new HashMap<Class<?>, String>();
         addPlayerPromptString.put(v.getClass(), "Position (1 = attacker | 2 = defender ):-");
         addPlayerPromptString.put(f.getClass(),
                 "Position (1 = goal keeper | 2 = defender | 3 = midfielder | 4 = forward):-");
-
+        // - `addPlayerPosChoice`: Maps `Class` objects to arrays of integers
+        // representing choices for player positions.
         HashMap<Class<?>, int[]> addPlayerPosChoice = new HashMap<Class<?>, int[]>();
         int[] volleyPos = new int[] { 1, 2 };
         int[] footballPos = new int[] { 1, 2, 3, 4 };
         addPlayerPosChoice.put(v.getClass(), volleyPos);
         addPlayerPosChoice.put(f.getClass(), footballPos);
-
+        // - `volleyPositionNameHM` and `footballPositionNameHM`: Maps arrays of
+        // integers (player positions) to arrays of strings (position names).
         HashMap<int[], String[]> volleyPositionNameHM = new HashMap<int[], String[]>();
         String[] volleyPosStrings = new String[] { "attacker", "defender" };
         volleyPositionNameHM.put(volleyPos, volleyPosStrings);
@@ -49,11 +59,13 @@ public class Main {
         HashMap<int[], String[]> footballPositionNameHM = new HashMap<int[], String[]>();
         String[] footballPosStrings = new String[] { "goal keeper", "defender", "midfielder", "forward" };
         footballPositionNameHM.put(footballPos, footballPosStrings);
-
+        // - `classPosNamHM`: Maps `Class` objects to the corresponding position name
+        // maps.
         HashMap<Class<?>, HashMap<int[], String[]>> classPosNamHM = new HashMap<Class<?>, HashMap<int[], String[]>>();
         classPosNamHM.put(v.getClass(), volleyPositionNameHM);
         classPosNamHM.put(f.getClass(), footballPositionNameHM);
-
+        // - `cFactories`: Maps strings representing command codes to various
+        // `CommandFactory` objects responsible for creating specific commands.
         HashMap<String, CommandFactory> cFactories = new HashMap<String, CommandFactory>();
 
         cFactories.put("c", new CreateTeamCommandFactory(sc, Teams, currTeam, undos, createTeamF, createTeamString));
@@ -71,7 +83,8 @@ public class Main {
         cFactories.put("r", new RedoCommandFactory(undos, redos));
         cFactories.put("l", new displayUndoRedoCommandFactory(undos, redos));
         cFactories.put("x", new ExitCommandFactory());
-
+        // - The code enters a while loop where it repeatedly displays a menu for the
+        // user to interact with the STMS.
         while (true) {
             System.out.println(
                     "Sport Teams Management System (STMS)\nc = create team, g = set current team, a = add player, m = modify player’s position, d = delete player, s = show team, p = display all teams, t = change team’s name, u = undo, r = redo, l = list undo/redo, x = exit system");
@@ -79,6 +92,10 @@ public class Main {
             if (currTeam[0] != null) {
                 System.out.println("The current team is " + currTeam[0].getTeamID() + " " + currTeam[0].getName());
             }
+            // - The user can enter different command codes to perform operations such as
+            // creating a team, adding players, modifying player positions, etc.
+            // - The appropriate factory object is used to create the corresponding command
+            // based on the user's input.
             System.out.println("Please enter command [ c | g | a | m | d | s | p | t | u | r | l | x ] :-");
             command = sc.nextLine().toLowerCase().trim();
             switch (command) {
